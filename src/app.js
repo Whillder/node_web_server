4b1f449b267d3199ab478261317add10b7a734d7
Whillder Escalante <whillderescalante@gmail.com>
2024-08-28 09:42:32 -0300

    Realizamos modificaciones para que funcione con importaciones y exportaciones de tipo ES6

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const {envs} = require('./config/env')
-const {startServer}=require('./server/server')
+import {envs} from './config/env.js'
+import {startServer} from './server/server.js'
+
 
 
 const main = () => {
@@ -9,7 +10,7 @@ const main = () => {
             public_path: envs.PUBLIC_PATH
         }
     )
-}
+};
 
 
 //Función agnóstica autoconvocada
@@ -17,4 +18,4 @@ const main = () => {
 //Autoconvocada porque la ejecutamos con los parentesis
 (async() =>{
     main()
-})()
+})();
